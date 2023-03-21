f02aaa8ecf35f74075e5adaa0b53e5b2e6c5d15f
Erick Malaguez Rowedder <eduardazingano@gmail.com>
2023-03-21 13:48:23 -0300

    Adicionando arquivos faltantes

--- exercicioLogica1/src/main/java/org/example/Main.java
+++ exercicioLogica1/src/main/java/org/example/Main.java
@@ -1,16 +1,8 @@
 package org.example;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        int idade = 0;
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Digite sua idade");
-        idade = teclado.nextInt();
 
-        if(idade < 18 ) {
-            System.out.println("você ainda é menor de idade");
-        } else {
-            System.out.println("você já é maior de idade");
-        }
+public class Main {
+
+    public static void main(String[] args) {
+     VerificaIdade verificaIdade = new VerificaIdade();
     }
-}
+ }

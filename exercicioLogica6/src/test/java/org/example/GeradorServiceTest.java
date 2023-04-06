ea8390417739a5afd993745eb2cf13c362c969e6
Eduarda Zingano <90721669+EduardaZingano@users.noreply.github.com>
2023-04-06 11:03:05 -0300

    Alterações pós feedback nos testes

--- exercicioLogica6/src/test/java/org/example/GeradorServiceTest.java
+++ exercicioLogica6/src/test/java/org/example/GeradorServiceTest.java
@@ -9,8 +9,7 @@ import java.util.Arrays;
 class GeradorServiceTest {
     GeradorService geradorService = new GeradorService();
     private int [] arrayTeste = new int[10];
-    private int menorValor;
-    private int maiorValor;
+   
 
     @Test
     void verificaSeGeraArray() {
@@ -28,14 +27,14 @@ class GeradorServiceTest {
 
     @Test
     void verificaNumeroMaior() {
-        maiorValor = Arrays.stream(geradorService.geradorLista()).max().getAsInt();
+      int  maiorValor = Arrays.stream(geradorService.geradorLista()).max().getAsInt();
 
         Assert.assertEquals(maiorValor, geradorService.pegaMaiorNumero());
     }
 
     @Test
     void verificaNumeroMenor() {
-        menorValor = Arrays.stream(geradorService.geradorLista()).min().getAsInt();
+      int  menorValor = Arrays.stream(geradorService.geradorLista()).min().getAsInt();
 
         Assert.assertEquals(menorValor, geradorService.pegaMenorNumero());
     }

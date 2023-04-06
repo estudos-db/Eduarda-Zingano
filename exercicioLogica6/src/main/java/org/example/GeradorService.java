03e6ec6a09452fb2fcd7088c4d53382df9dd41c2
Eduarda Zingano <90721669+EduardaZingano@users.noreply.github.com>
2023-04-06 11:02:02 -0300

    Alterações pós feedback

--- exercicioLogica6/src/main/java/org/example/GeradorService.java
+++ exercicioLogica6/src/main/java/org/example/GeradorService.java
@@ -3,28 +3,25 @@ package org.example;
 import java.util.*;
 
 public class GeradorService {
-    private int maior;
-    private int menor;
-    private int tamanho;
     private int lista[] = new int[10];
-    private int tamanhoArr = lista.length;
+    
 
     public int[] geradorLista() {
         Random aleatorios = new Random();
-        for (tamanho = 0; tamanho < tamanhoArr; tamanho++) {
+        for (int tamanho = 0; tamanho < lista.length; tamanho++) {
             lista[tamanho] = aleatorios.nextInt(101);
         }
         System.out.println(Arrays.toString(lista));
         return lista;
     }
     public int pegaMenorNumero() {
-        menor =  Arrays.stream(lista).min().getAsInt();
+        int menor =  Arrays.stream(lista).min().getAsInt();
         System.out.println("Menor número é: " + menor);
         return menor;
     }
 
     public int pegaMaiorNumero() {
-        maior = Arrays.stream(lista).max().getAsInt();
+       int maior = Arrays.stream(lista).max().getAsInt();
         System.out.println("Maior número é: " + maior);
         return maior;
     }

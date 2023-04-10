44a89ffab1341c065c17dd113cd78a8281296ed3
Erick Malaguez Rowedder <eduardazingano@gmail.com>
2023-04-10 11:17:25 -0300

    adicionando mais um caso de teste

--- exercicioLogica7/src/test/java/org/example/FibonacciServiceTest.java
+++ exercicioLogica7/src/test/java/org/example/FibonacciServiceTest.java
@@ -8,12 +8,22 @@ import java.util.Arrays;
 
 public class FibonacciServiceTest {
     FibonacciService fibonacciService = new FibonacciService();
-    private int [] arraySequenciaTest = new int[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+    private int [] arraySequenciaTest = new int[]{};
 
     @Test
 
-    public void testaNumerosDoArray() {
-
+   public void testaNumerosDoArrayDeTamanhoNove() {
+        arraySequenciaTest = new int[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+        
         Assert.assertEquals(Arrays.toString(arraySequenciaTest),Arrays.toString(fibonacciService.calculaSequenciaFibo(9)));
+
     }
+    @Test
+    public void testaNumerosDoArrayDeTamanhoDez() {
+        arraySequenciaTest = new int[]{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+
+        Assert.assertEquals(Arrays.toString(arraySequenciaTest),Arrays.toString(fibonacciService.calculaSequenciaFibo(10)));
+
+    }
+
 }

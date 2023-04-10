b24a8aa26998ef795ea5da3e4d3b577bfe34cf50
Erick Malaguez Rowedder <eduardazingano@gmail.com>
2023-04-10 13:09:13 -0300

    adicionando exercicio 12 + testes

---
+++ exercicioLogica12/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        VetorService vetorService = new VetorService();
+
+        vetorService.criaVetorUm();
+        vetorService.criaVetorDois();
+        vetorService.juntaVetores();
+        vetorService.ordenaVetores();
+
+    }
+}
